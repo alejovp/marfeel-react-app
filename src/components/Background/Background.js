8992ea4552a2a6d3a9be5d2dd62b5b69fe8e7dc7
Alejandro <alejovp@gmail.com>
2020-11-25 00:33:05 +0100

    Background import fix

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyledBackground } from './StyledBackGround';
+import { StyledBackground } from './StyledBackground';
 
 
 export const Background = ({ children, ...rest }) => {
